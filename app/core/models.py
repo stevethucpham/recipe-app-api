5bff38126338ea81273677b1a8aaf15e417ffed9
Thuc <dpham@datagene.com.au>
2020-05-14 10:19:52 +1000

    Ignore and fix Flake8 Rules

--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-     PermissionsMixin
+    PermissionsMixin
+
 
 class UserManager(BaseUserManager):
-    
+
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new user"""
         if not email:
@@ -34,4 +35,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
     # Customize username field as email
     USERNAME_FIELD = 'email'
-
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -11,10 +12,9 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-    
+
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@LONDONAPPDEV.com'
