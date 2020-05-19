fce4f31d44a293b8e951a0721c44ba9d57394560
Thuc <dpham@datagene.com.au>
2020-05-20 00:24:09 +1000

    Added list recipe feature.

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -3,10 +3,11 @@ from rest_framework.routers import DefaultRouter
 
 from recipe import views
 
-router = DefaultRouter()
 
+router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
